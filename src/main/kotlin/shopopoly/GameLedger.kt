3e76e4c381b2780e9000bbaabddb60d3b66c1628
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-09-21 14:13:02 +0100

    Adds payAward() method to GameLedger

--- src/main/kotlin/shopopoly/GameLedger.kt
+++ src/main/kotlin/shopopoly/GameLedger.kt
@@ -3,8 +3,22 @@ package shopopoly
 internal class GameLedger {
     val entries = mutableListOf<Entry>()
 
-    fun transferStartingBalance(amount: Int, player: String) {
-        entries.add(Entry(-amount, "Bank", player, "Starting balance transfer"))
+    fun transferStartingBalance(amount: Int, player: Player) {
+        entries.add(Entry(
+            amount = -amount,
+            from = "Bank",
+            to = player.name,
+            reason = "Starting balance transfer"
+        ))
+    }
+
+    fun payAward(location: Location, player: Player) {
+        entries.add(Entry(
+            amount = -location.visitorFeeOrAward,
+            from = "Bank",
+            to = player.name,
+            reason = "Award fee"
+        ))
     }
 }
 
--- src/test/kotlin/shopopoly/GameLedgerTest.kt
+++ src/test/kotlin/shopopoly/GameLedgerTest.kt
@@ -2,15 +2,19 @@ package shopopoly
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import io.mockk.every
+import io.mockk.mockk
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 
 object GameLedgerTest : Spek({
-    describe("GameLedger") {
+    describe("transferStartingBalance()") {
         val gameLedger = GameLedger()
-        gameLedger.transferStartingBalance(100, "Matt")
+        val player = mockk<Player>()
+        every { player.name } returns "Matt"
+        gameLedger.transferStartingBalance(100, player)
 
-        it("should return a list of entries") {
+        it("should add an entry to GameLedger") {
             assertThat(gameLedger.entries.size).isEqualTo(1)
         }
 
@@ -21,7 +25,32 @@ object GameLedgerTest : Spek({
 
         it("should record transfer of starting balance from 'Bank' to player 'Matt'") {
             assertThat(gameLedger.entries[0].from).isEqualTo("Bank")
-            assertThat(gameLedger.entries[0].to).isEqualTo("Matt")
+            assertThat(gameLedger.entries[0].to).isEqualTo(player.name)
+        }
+    }
+
+    // Bank paying a fee to a `Player`, e.g. when the player passes through the `Go` location.
+    describe("payAward()") {
+        val gameLedger = GameLedger()
+        val player = mockk<Player>()
+        every { player.name } returns "Matt"
+
+        val mockGoLocation = mockk<Go>()
+        every { mockGoLocation.visitorFeeOrAward } returns 100
+        gameLedger.payAward(mockGoLocation, player)
+
+        it("should add entry to GameLedger") {
+            assertThat(gameLedger.entries.size).isEqualTo(1)
+        }
+
+        it("should record award of £100 for passing Go") {
+            assertThat(gameLedger.entries[0].amount).isEqualTo(-100)
+            assertThat(gameLedger.entries[0].reason).isEqualTo("Award fee")
+        }
+
+        it("should record award from 'Bank' to player 'Matt'") {
+            assertThat(gameLedger.entries[0].from).isEqualTo("Bank")
+            assertThat(gameLedger.entries[0].to).isEqualTo(player.name)
         }
     }
 })
