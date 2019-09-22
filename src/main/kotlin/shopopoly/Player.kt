86165e601d096006902151d793b26856479de8cf
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-09-22 11:43:18 +0100

    Refactors to use Player class from challenge 10

--- src/main/kotlin/shopopoly/Player.kt
+++ src/main/kotlin/shopopoly/Player.kt
@@ -1,3 +1,23 @@
 package shopopoly
 
-internal class Player(val name: String)
+internal class Player(
+    val name: String,
+    val boardLocation: Int = 1,
+    val passedGo: Boolean = false
+) {
+
+    fun move(diceScore: Int = Dice().score()): Player {
+        val calculateBoardLocation = this.boardLocation + diceScore
+        val newBoardLocation =
+            when (calculateBoardLocation > 13) {
+                true -> calculateBoardLocation - 13
+                false -> calculateBoardLocation
+            }
+
+        return Player(
+            name = this.name,
+            boardLocation = newBoardLocation,
+            passedGo = calculateBoardLocation > 13
+        )
+    }
+}
--- src/test/kotlin/shopopoly/PlayerTest.kt
+++ src/test/kotlin/shopopoly/PlayerTest.kt
@@ -2,6 +2,8 @@ package shopopoly
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import assertk.assertions.isFalse
+import assertk.assertions.isTrue
 import io.mockk.every
 import io.mockk.mockk
 import org.spekframework.spek2.Spek
@@ -9,10 +11,45 @@ import org.spekframework.spek2.style.specification.describe
 
 object PlayerTest : Spek({
 
-    describe("Player") {
+    describe("player") {
+
         it("should have a name") {
-            val player = Player("Matt")
-            assertThat(player.name).isEqualTo("Matt")
+            assertThat(Player("Matt", 6).name).isEqualTo("Matt")
+        }
+
+        it("should have a board location") {
+            assertThat(Player("Matt", 6).boardLocation).isEqualTo(6)
+        }
+    }
+
+    describe("move") {
+
+        val diceMock = mockk<Dice>()
+        every { diceMock.score() } returns(7)
+
+        val diceMock2 = mockk<Dice>()
+        every { diceMock2.score() } returns(4)
+
+        it("should add dice roll score to boardLocation") {
+            val player = Player("Matt", 5)
+            assertThat(player.move(diceMock.score()).boardLocation).isEqualTo(12)
+        }
+
+        it("should restart boardLocation to 1 after moving past 13") {
+            val player = Player("Matt", 12)
+            assertThat(player.move(diceMock.score()).boardLocation).isEqualTo(6)
+        }
+
+        it("should set passedGo to true if location 13 is passed") {
+            val player = Player("Matt", 12)
+            assertThat(player.move(diceMock.score()).passedGo).isTrue()
+        }
+
+        it("should not set passedGo to true if location 13 is not passed") {
+            val player = Player("Matt", 2)
+            player.move(diceMock2.score())
+
+            assertThat(player.passedGo).isFalse()
         }
     }
 })
